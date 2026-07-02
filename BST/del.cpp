196782e538867d465c46ed1e005cce6a10dbac84
Jasraj <jasraj321.js@gmail.com>
2026-07-02 22:26:38 +0530

    BST day 1 done

---
+++ BST/del.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(!root)
+            return nullptr;
+        if(root->val==key){
+            if(!root->left&&!root->right){
+                delete root;
+                return nullptr;
+            }
+            else if(!root->left){
+                return root->right;
+            }
+            else if(!root->right){
+                return root->left;
+            }
+            else
+            {
+                TreeNode* temp=root->right;
+                while(temp->left)
+                    temp=temp->left;
+                root->val=temp->val;
+                root->right=deleteNode(root->right,root->val);
+            }
+        }
+        if(root->val>key)
+            root->left=deleteNode(root->left,key);
+        if(root->val<key)
+            root->right=deleteNode(root->right,key);
+        return root;
+    }
+};
